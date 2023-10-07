d99f6640798e9e5f709dd7b255b57bfe84f79442
Brian Yu <destiny187306196@gmail.com>
2023-10-06 21:38:42 -0400

    Added functionality to create, update and delete posts

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,14 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_swagger import swagger
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f15bece435da232d6b42e722081eb4a7'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
+
 bcrypt = Bcrypt(app)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' # this is where the viewer will be redirected if accessing pages that requied login
 login_manager.login_message_category = 'info'
+
+swg = swagger(app)
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,27 +1,16 @@
-from flask import render_template, url_for, flash, redirect, request
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
-from flaskblog.models import User
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-posts = [
-    {
-        'author': "Author 1",
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2023'
-    },
-    {
-        'author': "Author 2",
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'June 20, 2023'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -64,7 +53,85 @@ def logout():
     logout_user() # user is not required since it is to log out the current user
     return redirect(url_for('home'))
 
-@app.route("/account")
+
+def save_picture(form_picture, output_size: tuple = (125, 125)) -> str:
+    # Prepare image name and path
+    _, suffix = os.path.splitext(form_picture.filename)
+    picture_fn = secrets.token_hex(8) + suffix
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    # Resize the image to reduce volume
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    # Store to desired path
+    i.save(picture_path)
+    return picture_fn
+
+@app.route("/account", methods = ['GET', 'POST'])
 @login_required # this decorator will enforce the user to login before can access
 def account():
-    return render_template('account.html', title = 'Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+        current_user.username = form.username.data
+        current_user.email = form.email.data.lower()
+        current_user.image_file = picture_file
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for("static", filename = f'profile_pics/{current_user.image_file}')
+    return render_template('account.html', title = 'Account', 
+                           image_file = image_file, form = form)
+
+@app.route("/post/new", methods = ['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title = form.title.data, content = form.content.data, author = current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template("create_post.html", title = "New Post", form = form, legend = 'New Post')
+
+@app.route("/post/<int:post_id>")
+def post(post_id: int):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title = post.title, post = post)
+
+@app.route("/post/<int:post_id>/update", methods = ['GET', 'POST'])
+@login_required
+def update_post(post_id: int):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated!", 'success')
+        return redirect(url_for('post', post_id = post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template("create_post.html", title = "Update Post", form = form, legend = 'Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods = ['POST'])
+@login_required
+def delete_post(post_id: int):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted!", 'success')
+
+    return redirect(url_for('home'))
